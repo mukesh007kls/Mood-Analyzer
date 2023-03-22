15d4b1e321a145001caffe8b672615f0186491a0
mukesh007kls <mallikanti2000@gmail.com>
2023-03-23 00:07:17 +0530

    REFACTORED:Test Cases

--- src/test/java/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyserTest.java
@@ -8,21 +8,21 @@ public class MoodAnalyserTest {
     @Test
     public void givenMoodShouldReturnHappy(){
         MoodAnalyser moodAnalyser=new MoodAnalyser();
-        String mood=moodAnalyser.analyserMood("Happy");
+        String mood=moodAnalyser.analyserMood("I am in Happy mood");
         assertEquals("HAPPY",mood);
     }
 
     @Test
     public void givenMoodShouldReturnSad(){
         MoodAnalyser moodAnalyser=new MoodAnalyser();
-        String mood=moodAnalyser.analyserMood("Sad");
+        String mood=moodAnalyser.analyserMood("i am in Sad mood");
         assertEquals("SAD",mood);
     }
 
     @Test
     public void givenMoodShouldReturnIncorrect(){
         MoodAnalyser moodAnalyser=new MoodAnalyser();
-        String mood=moodAnalyser.analyserMood("fine");
+        String mood=moodAnalyser.analyserMood("i am fine");
         assertEquals("Incorrect mood",mood);
     }
 }
